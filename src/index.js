ab2f0bad7fbfb5c84204c7aa875b9d1411edacb4
Heber Quequejana <heberqc@gmail.com>
2018-12-22 13:33:30 -0500

    :construction: refactor(routes): configure authentication, links, public routes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.engine('.hbs', exphbs({
 	layoutsDir: path.join(app.get('views'), 'layouts'),
 	partialsDir: path.join(app.get('views'), 'partials'),
 	extname: '.hbs',
-	helpers: require('./lib/handlebars')
+	helpers: require('./lib/handlebars'),
 }))
 app.set('view engine', '.hbs')
 
@@ -24,11 +24,17 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // global variables
+app.use((req, res, next) => {
+	next()
+})
 
 // routes
 app.use(require('./routes'))
+app.use(require('./routes/authentication'))
+app.use('/links', require('./routes/links'))
 
 // public
+app.use(express.static(path.join(__dirname, 'public')))
 
 // starting the server
 app.listen(app.get('port'), () => {
